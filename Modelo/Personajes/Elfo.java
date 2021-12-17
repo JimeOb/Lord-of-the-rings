6234e7dc4a5d951cf1ee144ad3ecd607e4ad4080
SantiagoGarcia8 <72152591+SantiagoGarcia8@users.noreply.github.com>
2021-12-17 01:40:50 -0500

    Add files via upload

--- Modelo/Personajes/Elfo.java
+++ Modelo/Personajes/Elfo.java
@@ -1,5 +1,19 @@
 package Modelo.Personajes;
 
+import java.util.Random;
+
+import Modelo.Armaduras.Armadura;
+import Modelo.Armas.Arma;
+import Modelo.Montura.Montura;
+
 public class Elfo extends Personaje {
-    
+
+    public Elfo (Arma arma, Armadura armadura, Montura montura){
+        super(arma, armadura, montura);
+        Random rm = new Random();
+        this.altura = 2.0 + rm.nextDouble();
+        this.edad = 200 + rm.nextInt(400);
+        this.especialidad = "Arquero y cazador";
+        this.raza = "Elfo";
+    }
 }

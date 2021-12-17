f3387f138537eb2f2b971adc73f9e5c287263d4c
SantiagoGarcia8 <sjgarciat@correo.udistrital.edu.co>
2021-12-17 10:03:19 -0500

    Creación de clases equipamientoFactory

---
+++ modelo/equipamientos_factory/HobbitEquipamientoFactory.java
@@ -0,0 +1,27 @@
+package modelo.equipamientos_factory;
+
+import modelo.armaduras.Armadura;
+import modelo.armaduras.ArmaduraLigera;
+import modelo.armas.Arma;
+import modelo.armas.Espada;
+import modelo.montura.Caballo;
+import modelo.montura.Montura;
+
+
+public class HobbitEquipamientoFactory implements PersonajeEquipamientoFactory {
+
+    @Override
+    public Arma createArma() {        
+        return new Espada("Comun");
+    }
+
+    @Override
+    public Armadura createArmadura() {        
+        return new ArmaduraLigera("Mala");
+    }
+
+    @Override
+    public Montura createMontura() {        
+        return new Caballo();
+    }
+}
---
+++ modelo/equipamientos_factory/OrcoEquipamientoFactory.java
@@ -0,0 +1,27 @@
+package modelo.equipamientos_factory;
+
+import modelo.armaduras.Armadura;
+import modelo.armaduras.ArmaduraPesada;
+import modelo.armas.Arma;
+import modelo.armas.Hacha;
+import modelo.montura.Bestia;
+import modelo.montura.Montura;
+
+public class OrcoEquipamientoFactory implements PersonajeEquipamientoFactory {
+
+    @Override
+    public Arma createArma() {        
+        return new Hacha("Poco comun");
+    }
+
+    @Override
+    public Armadura createArmadura() {        
+        return new ArmaduraPesada("Superior");
+    }
+
+    @Override
+    public Montura createMontura() {        
+        return new Bestia();
+    }
+    
+}

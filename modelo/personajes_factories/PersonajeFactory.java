959854b4d227f720f80b18160b7c41795096405b
SantiagoGarcia8 <72152591+SantiagoGarcia8@users.noreply.github.com>
2022-01-27 11:33:00 -0500

    Delete modelo/personajes_factories directory

--- modelo/personajes_factories/PersonajeFactory.java
+++
@@ -1,9 +0,0 @@
-package modelo.personajes_factories;
-
-import modelo.personajes.Personaje;
-
-public abstract class PersonajeFactory {
-    
-    public abstract Personaje crearPersonaje();
-    
-}
